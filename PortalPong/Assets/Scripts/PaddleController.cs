a181eeebfb2ed93efaafc04701bac89209f68934
Avery Pratt <apratt91@gmail.com>
2018-01-27 00:33:45 -0800

    added paddle with friction (not using unity physics)

--- PortalPong/Assets/Scripts/PaddleController.cs
+++ PortalPong/Assets/Scripts/PaddleController.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PaddleController : MonoBehaviour {
+public class PaddleController : MonoBehaviour
+{
+    public float speed;
+    [Range(0, 1)]
+    public float friction;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float vel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        BallController ball = other.GetComponent<BallController>();
+        Rigidbody rbd = ball.rbd;
+        Vector3 force = other.transform.right * ball.rbd.velocity.magnitude * 2;
+        Vector3 newForce = Vector3.Lerp(force, new Vector3(0, vel * friction), 0.5f) * 2;
+        rbd.velocity += newForce;
+    }
+
+    private void Update()
+    {
+        vel = 0;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(transform.up * speed * Time.deltaTime);
+            vel += speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-transform.up * speed * Time.deltaTime);
+            vel -= speed;
+        }
+    }
 }
