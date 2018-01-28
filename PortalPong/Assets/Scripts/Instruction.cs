863441c0e8022aaf19411a880b8214892651bd0e
pacificyost <pacificyost@gmail.com>
2018-01-28 11:55:44 -0800

    Instruction scene
    Added Instruction scene with transition to next level after 15 seconds
    or if enter key is pressed.  Also moved Audio Manager script into
    scripts folder.

---
+++ PortalPong/Assets/Scripts/Instruction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instruction : MonoBehaviour {
+    public float loadSceneDelay = 15.0f;
+
+    private void Awake()
+    {
+        Invoke("LoadNextLevel", loadSceneDelay);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    void LoadNextLevel()
+    {
+        SceneController.LoadNextLevel();
+    }
+}
