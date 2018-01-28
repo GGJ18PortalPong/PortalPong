4426be0dc77d22eadc12a7b327fc9f4dde344434
Avery Pratt <apratt91@gmail.com>
2018-01-27 16:05:34 -0800

    pushing

--- PortalPong/Assets/Scripts/PortalConnection.cs
+++ PortalPong/Assets/Scripts/PortalConnection.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PortalConnection : MonoBehaviour {
-
+public class PortalConnection : MonoBehaviour
+{
     public GameObject destination;
-
-
 }
